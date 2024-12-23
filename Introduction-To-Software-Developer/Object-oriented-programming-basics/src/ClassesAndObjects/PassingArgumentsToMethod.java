cfb75839b87aa190c74f5604781aec87837da8ff
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2024-12-23 20:07:29 +0700

    passing arguments to method example

---
+++ Introduction-To-Software-Developer/Object-oriented-programming-basics/src/ClassesAndObjects/PassingArgumentsToMethod.java
@@ -0,0 +1,29 @@
+package ClassesAndObjects;
+
+public class PassingArgumentsToMethod {
+
+    public static int calculatePunchDamage(int strength) {
+        return strength * 2;
+    }
+
+    public static int calculateSpecialAttackDamage(int strength, String attackType){
+        int damage = strength;
+        if (attackType.equals("Fire Punch")){
+            damage *= 3;
+        } else if (attackType.equals("Ice Blast")) {
+            damage *= 2;
+        }
+        return damage;
+    }
+
+    public static void main(String[] args){
+        int villainHealth = 100;
+        int punchStrength = 20;
+        String specialAttack = "Fire Punch";
+
+        int remainingHealth = villainHealth - calculatePunchDamage(punchStrength);
+        int damageBySpecialAttack = calculateSpecialAttackDamage(punchStrength, specialAttack);
+        System.out.println("Villain's remaining health: " + remainingHealth);
+    }
+
+}
