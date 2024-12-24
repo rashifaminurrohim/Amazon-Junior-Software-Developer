4ff1a7e879f2c2a508d643ded1ea9fdcff509520
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2024-12-24 21:24:00 +0700

    making bmi calculator with grouping methods

---
+++ Introduction-To-Software-Developer/Object-oriented-programming-basics/src/ClassesAndObjects/BMICalculator.java
@@ -0,0 +1,11 @@
+package ClassesAndObjects;
+
+public class BMICalculator {
+    public double calculateBmiImperial(double weightInPounds, double heightInInches){
+        return weightInPounds / (heightInInches * heightInInches) * 703;
+    }
+
+    public double calculateBmiMetrics(double weightInKilos, double heightInMeters){
+        return weightInKilos / (heightInMeters * heightInMeters);
+    }
+}
---
+++ Introduction-To-Software-Developer/Object-oriented-programming-basics/src/ClassesAndObjects/MainBMICalculator.java
@@ -0,0 +1,19 @@
+package ClassesAndObjects;
+
+public class MainBMICalculator {
+    public static void main(String[] args){
+        BMICalculator bmiCalculator = new BMICalculator();
+
+        double weightInPounds = 154.0;
+        double heightInInches = 70.0;
+        double bmiImperial = bmiCalculator.calculateBmiImperial(weightInPounds, heightInInches);
+
+        double weightInKilos = 70.0;
+        double heightInMeters = 1.82;
+        double bmiMetrics = bmiCalculator.calculateBmiMetrics(weightInKilos, heightInMeters);
+
+        System.out.println("BMI in Imperial system: " + bmiImperial);
+        System.out.println("BMI in Metrics system: " + bmiMetrics);
+
+    }
+}
