fb7655d9e8a783631c1b85ff51b2fe5ed780996e
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2024-12-25 09:37:27 +0700

    learning constructor : default constractor, no-arguments constructor, and parameterized constructor

--- Introduction-To-Software-Developer/Object-oriented-programming-basics/src/ClassesAndObjects/Car.java
+++ Introduction-To-Software-Developer/Object-oriented-programming-basics/src/ClassesAndObjects/Car.java
@@ -1,10 +1,29 @@
 package ClassesAndObjects;
 
 public class Car {
+    // Attributes (member variables) define a Car's properties
     String make; // stores the car's make (like Ford, Toyota)
     String model; // stores the car's specific model name (like Mustang, Camry)
     String color; // stores the car's color (like Red, Silver)
     int year; // stores the car's manufacturing year (like 2020)
+
+    // no-arguments constructor (no parameters)
+    public Car(){
+        // The this keyword is used within a constructor to refer to the current object being created.
+        this.make = "Unknown";
+        this.model = "Unknown";
+        this.color = "no color";
+        this.year = 0;
+    }
+
+    // parameterized constructor to initialize with specific values
+    public Car(String make, String model, String color, int year){
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
     public String getCarDetails(){
         // build, and return a string by adding each car property with a label
         // and using new line character \n
