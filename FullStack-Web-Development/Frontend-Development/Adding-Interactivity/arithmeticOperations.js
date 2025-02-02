4a82f70e2af4165bd1931042201c2aee050653a0
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-02-02 20:51:40 +0700

    Arithmetic Operations in JavaScript

---
+++ FullStack-Web-Development/Frontend-Development/Adding-Interactivity/arithmeticOperations.js
@@ -0,0 +1,27 @@
+/** Arithmetic Operations in JavaScript
+ * Addition(+)
+ * Subtraction(-)
+ * Multiplication(*)
+ * Division(/)
+ * Modulus(%)
+ */
+function arithmeticOperation() {
+  // Example: Arithmetic Operations
+  let num1 = 10;
+  let num2 = 5;
+
+  let sum = num1 + num2; // Addition
+  let difference = num1 - num2; // Subtaction
+  let product = num1 * num2; // Multiplication
+  let quotient = num1 / num2; // Division
+  let remainder = num1 % num2; // Modulus
+
+  console.log("Number 1:", num1);
+  console.log("Number 2:", num2);
+
+  console.log("Sum:", sum);
+  console.log("Difference:", difference);
+  console.log("Product:", product);
+  console.log("Quotient:", quotient);
+  console.log("Remainder:", remainder);
+}
