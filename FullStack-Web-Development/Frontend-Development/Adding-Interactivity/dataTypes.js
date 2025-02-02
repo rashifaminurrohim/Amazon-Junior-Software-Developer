e20144d536255daec13f52ed69d5c82273171850
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-02-02 20:37:58 +0700

    Data types in JavaScript

---
+++ FullStack-Web-Development/Frontend-Development/Adding-Interactivity/dataTypes.js
@@ -0,0 +1,20 @@
+/**
+ * Numbers: Numeric values
+ * Strings: Text values
+ * Boolean: Logical values (true or false)
+ * Undefined: Declared but not assigned a value
+ * Null: Explicit set to no value
+ */
+function invokeDataType() {
+  // Example: Data Types
+  let numberValue = 42; // Number
+  let stringValue = "Hello, World!"; // String
+  let booleanValue = true; // Boolean
+  let undefinedValue; // Undefined
+  let nullValue = null; // Null
+  console.log("numberValue:", numberValue);
+  console.log("stringValue:", stringValue);
+  console.log("booleanValue:", booleanValue);
+  console.log("undefinedValue:", undefinedValue);
+  console.log("nullValue", nullValue);
+}
