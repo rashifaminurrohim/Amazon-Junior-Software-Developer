90a58f3bdb75389b0848bd67b8b8667babcf0da2
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2025-02-04 10:27:59 +0700

    Renaming variables

---
+++ FullStack-Web-Development/Frontend-Development/Adding-Interactivity/variables.js
@@ -0,0 +1,7 @@
+function declareVariables() {
+  // Example: Variable Declaration
+  var myName = "Alice"; // Declaring a variable using 'var' -> Function/global scope
+  let age = 25; // Declaring a variable using 'let' -> Block-scoped
+  const country = "USA"; // Declaring a variable using 'const' -> Block-scoped & cannot be reassigned
+  console.log("Name: " + myName + ", Age: " + age + ", Country: " + country);
+}
